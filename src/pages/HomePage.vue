fae9940f1b3a133e09803a49fa46216f2a8b0556
Ivan Saklakov <sakla4onakne@mail.ru>
2023-04-10 14:42:01 +0900

    fix: добавил относительные пути для деплоя

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -13,7 +13,7 @@ export default {
       slides: [
         {
           id: 1,
-          image: '/images/racer.png',
+          image: './images/racer.png',
           description: `Для примера мы показали вам его лицо. В первой серии он прячется в подвале за мониторами,
             и пусть борода не собьёт вас с толку. Найдите героя и нажмите на паузу — ему не терпится
             отдать вам промокод.`,
@@ -21,7 +21,7 @@ export default {
         },
         {
           id: 2,
-          image: '/images/man2.png',
+          image: './images/man2.png',
           description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
@@ -29,7 +29,7 @@ export default {
         },
         {
           id: 3,
-          image: '/images/racer.png',
+          image: './images/racer.png',
           description: `Слайд 3`,
           url: '#3'
         }
